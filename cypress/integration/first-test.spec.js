f2e66618ea396eaec6ee9488621ba1b970f4aacd
Ana Marija <anamarijamirceska@gmail.com>
2024-11-05 13:29:05 +0100

    Added a comment

--- cypress/integration/first-test.spec.js
+++ cypress/integration/first-test.spec.js
@@ -1,3 +1,6 @@
+
+// Updated test file for new branch testing
+
 describe('My First Cypress Test', () => {
     it('Visits the Cypress website and checks the title', () => {
       cy.visit('https://www.cypress.io');
